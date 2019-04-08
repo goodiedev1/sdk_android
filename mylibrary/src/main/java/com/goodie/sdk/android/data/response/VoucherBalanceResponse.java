481a50623ef4f940e5a0114bf52b1080609b99bd
shu <rahmanshurahman@gmail.com>
2019-04-08 11:33:55 +0700

    list voucher balance

--- mylibrary/src/main/java/com/goodie/sdk/android/data/response/VoucherBalanceResponse.java
+++ mylibrary/src/main/java/com/goodie/sdk/android/data/response/VoucherBalanceResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class VoucherBalanceResponse extends GenericResponse{
 
-    @SerializedName("listVoucherbalance")
+    @SerializedName("listVoucherBalance")
     @Expose
     private List<VoucherBalanceResp> listVoucherbalance;
 
